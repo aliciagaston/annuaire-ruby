20bf1e6c742c9ca4b9440c6bad64cb2f157408b7
Alicia <alicia.gaston@etu.u-bordeaux.fr>
2018-06-06 12:01:35 +0200

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :update]
+
   def index
-		@users = User.all
+		@users = User.all.order(:first_name)
+  end
+  def load_user
+    @user = User.find params[:id]
+  end
+  def update
+    @user.update(params.require(:user).permit(:first_name, :last_name, :date_of_birth, :email, :phone_number))
+    redirect_to :action=>'show', :id => @user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
